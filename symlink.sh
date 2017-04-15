cb352ef075b635a9c93a95ac193279211a39fd68
iTakeshi <takeshi.ito.doraemon@gmail.com>
2017-04-15 18:55:12 +0900

    delete old script

--- symlink.sh
+++
@@ -1,5 +0,0 @@
-ln -sf $(pwd)/home/.gitconfig           ~/
-ln -sf $(pwd)/home/.bashrc              ~/
-ln -sf $(pwd)/home/.comfig/nvim         ~/.config
-ln -sf $(pwd)/home/.config/termite      ~/.config
-
