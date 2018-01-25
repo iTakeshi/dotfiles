5d6cc81a5148d65b196ac3ec2c678ab00524c87d
iTakeshi <takeshi.ito.doraemon@gmail.com>
2018-01-25 12:48:45 +0900

    restrict icon size of noutification

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -557,3 +557,5 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+naughty.config.defaults['icon_size'] = 100
