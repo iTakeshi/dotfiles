4299fb896bbd26e62d86c5bfb90a390f030786a5
iTakeshi <itakeshi.net@gmail.com>
2018-03-19 21:54:59 +0900

    awesome: rename conflicting shortcut

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -363,7 +363,7 @@ clientkeys = gears.table.join(
               {description = "move to master", group = "client"}),
     awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
               {description = "move to screen", group = "client"}),
-    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+    awful.key({ modkey, "Control" }, "t",      function (c) c.ontop = not c.ontop            end,
               {description = "toggle keep on top", group = "client"}),
     awful.key({ modkey,           }, "n",
         function (c)
