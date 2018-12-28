ec68054c8d0451b0044bc188cfb9b32fec40d496
iTakeshi <itakeshi.net@gmail.com>
2018-12-29 00:35:22 +0900

    set scrollback length for tym

--- home/.config/tym/config.lua
+++ home/.config/tym/config.lua
@@ -1,5 +1,6 @@
 local tym = require('tym')
 
 tym.set_config({
-    font = "FantasqueSansMono Nerd Font 13"
+    font              = "FantasqueSansMono Nerd Font 13",
+    scrollback_length = 10000,
 })
