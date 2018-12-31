c9a328770a10671b854646da54c596da92c60483
iTakeshi <itakeshi.net@gmail.com>
2018-12-31 20:27:47 +0900

    modified nord colorscheme for tym

--- home/.config/tym/theme.lua
+++ home/.config/tym/theme.lua
@@ -1,25 +1,44 @@
-return {
-    color_foreground        = "#d8dee9",
-    color_background        = "#2e3440",
-    color_cursor            = "#d8dee9",
-    color_cursor_foreground = "#2e3440",
-    color_highlight         = "#4c566a",
-    color_bold              = "#d8dee9",
+local fg      = "#ccd2dc"
+local bg      = "#22252f"
+local black   = "#525e73"
+local red     = "#bf616a"
+local green   = "#a3be8c"
+local yellow  = "#ebcb8b"
+local blue    = "#6ba4dc"
+local magenta = "#b48ead"
+local cyan    = "#67bcbb"
 
-    color_0                 = "#3b4252",
-    color_1                 = "#bf616a",
-    color_2                 = "#a3be8c",
-    color_3                 = "#ebcb8b",
-    color_4                 = "#81a1c1",
-    color_5                 = "#b48ead",
-    color_6                 = "#88c0d0",
-    color_7                 = "#e5e9f0",
-    color_8                 = "#4c566a",
-    color_9                 = "#bf616a",
-    color_10                = "#a3be8c",
-    color_11                = "#ebcb8b",
-    color_12                = "#81a1c1",
-    color_13                = "#b48ead",
-    color_14                = "#8fbcbb",
-    color_15                = "#eceff4",
+return {
+    color_foreground           = fg,
+    color_bold                 = fg,
+    color_cursor               = fg,
+    color_highlight            = fg,
+
+    color_background           = bg,
+    color_cursor_foreground    = bg,
+    color_highlight_foreground = bg,
+
+    color_0                    = black,
+    color_8                    = black,
+
+    color_1                    = red,
+    color_9                    = red,
+
+    color_2                    = green,
+    color_10                   = green,
+
+    color_3                    = yellow,
+    color_11                   = yellow,
+
+    color_4                    = blue,
+    color_12                   = blue,
+
+    color_5                    = magenta,
+    color_13                   = magenta,
+
+    color_6                    = cyan,
+    color_14                   = cyan,
+
+    color_7                    = fg,
+    color_15                   = fg,
 }
