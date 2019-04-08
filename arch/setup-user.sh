ae98418186e76540067dc70c5bef24d21238d48b
iTakeshi <itakeshi.net@gmail.com>
2019-04-08 18:17:54 +0900

    update setup-user.sh

--- arch/setup-user.sh
+++ arch/setup-user.sh
@@ -5,33 +5,36 @@ fi
 sudo sed -i -e "${multilib_line}s/^#//" /etc/pacman.conf
 sudo sed -i -e "$(expr $multilib_line + 1)s/^#//" /etc/pacman.conf
 
+sudo pacman -Syyuu
+sudo pacman -S git
+
+git clone https://github.com/iTakeshi/dotfiles.git $HOME/dotfiles
+ln -sf $HOME/dotfiles/home/.bash_logout .
+ln -sf $HOME/dotfiles/home/.bash_profile .
+ln -sf $HOME/dotfiles/home/.bashrc .
+ln -sf $HOME/dotfiles/home/.config .
+ln -sf $HOME/dotfiles/home/.myclirc .
+ln -sf $HOME/dotfiles/home/.xinitrc .
+ln -sf $HOME/dotfiles/home/.xprofile .
+mkdir -p $HOME/.local/share/bash
+mkdir -p $HOME/.local/share/python
+cd $HOME/dotfiles
+git remote set-url --push origin github:iTakeshi/dotfiles.git
+
 git clone https://aur.archlinux.org/yay.git /tmp/yay
 cd /tmp/yay
 makepkg -sri
 
-yay && yay -S \
-    arch-diff pacman-contrib pkgfile gdisk nfs-utils neovim git wget bat hexyl tym \
+yay -S \
+    arch-diff pacman-contrib pkgfile gdisk nfs-utils neovim wget bat hexyl tym \
     openssh openconnect bash-completion words \
     xsel unzip evtest udevil hwinfo ntp nginx mariadb hping htop smartmontools tcpdump \
     autoconf automake bazel cmake clang nasm gdb cloc docker docker-compose \
-    jre8-openjdk jdk8-openjdk rust cargo python-pip python2-pip scala sbt ammonite tk eigen
+    jre8-openjdk jdk8-openjdk python-pip python2-pip ruby scala sbt ammonite tk eigen
 
 pip install --user mycli numpy PyQt5 matplotlib pynvim
 pip2 install --user pynvim
 sed -i -e "/^backend/s/^\(backend[ ]\+: \).*/\1Qt5Agg/" $HOME/.local/lib/python3.7/site-packages/matplotlib/mpl-data/matplotlibrc
-
-cd $HOME
-git clone https://github.com/iTakeshi/dotfiles.git
-ln -sf /$HOME/dotfiles/home/.bash_logout .
-ln -sf /$HOME/dotfiles/home/.bash_profile .
-ln -sf /$HOME/dotfiles/home/.bashrc .
-ln -sf /$HOME/dotfiles/home/.config .
-ln -sf /$HOME/dotfiles/home/.myclirc .
-ln -sf /$HOME/dotfiles/home/.xinitrc .
-ln -sf /$HOME/dotfiles/home/.xprofile .
-mkdir /$HOME/.local/share/python
-cd $HOME/dotfiles
-git remote set-url --push origin github:iTakeshi/dotfiles.git
 
 sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo sed -i -e "/^\[mysqld\]$/a character-set-server = utf8" /etc/mysql/my.cnf
@@ -42,8 +45,14 @@ sudo systemctl enable ntpd
 sudo systemctl enable mysqld
 sudo systemctl enable devmon@$USER
 
+read -p 'Am I equipped with nvidia GPU(s)? [y/N] > ' setup_nvidia
+if [ "$setup_nvidia" == "Y" -o "$setup_nvidia" == "y" ]; then
+    yay -S nvidia nvidia-utils nvidia-settings cuda cudnn
+fi
+
 read -p 'setup GUI? [Y/n] > ' setup_gui
 if [ "$setup_gui" != "N" -a "$setup_gui" != "n" ]; then
+    gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
     yay -S \
         xorg-server xorg-xev xorg-xauth lightdm lightdm-gtk-greeter awesome \
         fcitx fcitx-mozc fcitx-configtool fcitx-im scrot fontforge wireshark-qt \
