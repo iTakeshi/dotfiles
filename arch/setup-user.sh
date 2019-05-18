da3b478ddf99c76cbf0cc3f25f0997ac86082071
iTakeshi <itakeshi.net@gmail.com>
2019-05-18 12:59:49 +0900

    xinitrc is not used by lightdm

--- arch/setup-user.sh
+++ arch/setup-user.sh
@@ -14,7 +14,6 @@ ln -sf $HOME/dotfiles/home/.bash_profile .
 ln -sf $HOME/dotfiles/home/.bashrc .
 ln -sf $HOME/dotfiles/home/.config .
 ln -sf $HOME/dotfiles/home/.myclirc .
-ln -sf $HOME/dotfiles/home/.xinitrc .
 ln -sf $HOME/dotfiles/home/.xprofile .
 mkdir -p $HOME/.local/share/bash
 mkdir -p $HOME/.local/share/python
