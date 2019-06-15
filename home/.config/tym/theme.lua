43cb02fe97ac0673a126963f847b0dad8584c361
iTakeshi <itakeshi.net@gmail.com>
2019-06-15 17:54:48 +0900

    vim & tym colorscheme "pastel"

--- home/.config/tym/theme.lua
+++ home/.config/tym/theme.lua
@@ -1,12 +1,20 @@
-local fg      = "#ccd2dc"
-local bg      = "#22252f"
-local black   = "#525e73"
-local red     = "#bf616a"
-local green   = "#a3be8c"
-local yellow  = "#ebcb8b"
-local blue    = "#6ba4dc"
-local magenta = "#b48ead"
-local cyan    = "#67bcbb"
+local fg        = "#ccd2dc"
+local bg        = "#292933"
+local black     = "#525266"
+local red       = "#e6aaaa"
+local red_d     = "#996767"
+local orange    = "#e6be96"
+local orange_d  = "#997a5c"
+local yellow    = "#e6e68c"
+local yellow_d  = "#999951"
+local green     = "#b9e6b9"
+local green_d   = "#679967"
+local blue      = "#8cc8e6"
+local blue_d    = "#518599"
+local purple    = "#b9b9e6"
+local purple_d  = "#7a7a99"
+local magenta   = "#e6b9e6"
+local magenta_d = "#997a99"
 
 return {
     color_foreground           = fg,
@@ -22,22 +30,22 @@ return {
     color_8                    = black,
 
     color_1                    = red,
-    color_9                    = red,
+    color_9                    = red_d,
 
     color_2                    = green,
-    color_10                   = green,
+    color_10                   = green_d,
 
     color_3                    = yellow,
-    color_11                   = yellow,
+    color_11                   = yellow_d,
 
     color_4                    = blue,
-    color_12                   = blue,
+    color_12                   = blue_d,
 
     color_5                    = magenta,
-    color_13                   = magenta,
+    color_13                   = magenta_d,
 
-    color_6                    = cyan,
-    color_14                   = cyan,
+    color_6                    = purple,
+    color_14                   = purple_d,
 
     color_7                    = fg,
     color_15                   = fg,
