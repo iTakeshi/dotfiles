07f00c810f23ed4fad01c904a76a46191ae8c925
iTakeshi <itakeshi.net@gmail.com>
2019-09-11 23:08:42 +0900

    tym autohide mouse cursor

--- home/.config/tym/config.lua
+++ home/.config/tym/config.lua
@@ -1,6 +1,7 @@
 local tym = require('tym')
 
 tym.set_config({
+    autohide          = true,
     font              = "FantasqueSansMono Nerd Font 13",
     scrollback_length = 10000,
     silent            = true,
