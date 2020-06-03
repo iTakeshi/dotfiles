5cb6698ba811411f331bbf3d766d025935f7db6b
iTakeshi <itakeshi.net@gmail.com>
2020-06-03 12:05:54 +0900

    tym font scale

--- home/.config/tym/config.lua
+++ home/.config/tym/config.lua
@@ -1,8 +1,17 @@
-local tym = require('tym')
+local tym = require("tym")
 
 tym.set_config({
     autohide          = true,
     font              = "FantasqueSansMono Nerd Font 13",
     scrollback_length = 10000,
     silent            = true,
+})
+
+tym.set_keymaps({
+  ["<Ctrl><Shift>Up"] = function()
+      tym.set("scale", tym.get("scale") + 20)
+  end,
+  ["<Ctrl><Shift>Down"] = function()
+      tym.set("scale", tym.get("scale") - 20)
+  end,
 })
