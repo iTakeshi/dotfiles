9e98523ff13924f579569fe6cf0136072974de3f
iTakeshi <itakeshi.net@gmail.com>
2021-03-05 18:17:52 +0900

    update setup-python script

--- arch/setup-python.sh
+++ arch/setup-python.sh
@@ -2,8 +2,22 @@
 
 python2_latest=$(pyenv install-latest --print 2)
 python3_latest=$(pyenv install-latest --print 3)
-pyenv install "$python2_latest"
-pyenv install "$python3_latest"
+
+if [[ ! $(pyenv versions | grep $python2_latest) ]]; then
+    pyenv install "$python2_latest"
+fi
+
+if [[ ! $(pyenv versions | grep $python3_latest) ]]; then
+    pyenv install "$python3_latest"
+fi
+
+if [[ $(pyenv versions | grep neovim2) ]]; then
+    pyenv virtualenv-delete -f neovim2
+fi
+
+if [[ $(pyenv versions | grep neovim3) ]]; then
+    pyenv virtualenv-delete -f neovim3
+fi
 
 pyenv virtualenv "$python3_latest" neovim3
 pyenv global neovim3
