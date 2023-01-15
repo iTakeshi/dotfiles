3fa74ef97cb73bdea64e134ad2571aaa5e217e2a
iTakeshi <itakeshi.net@gmail.com>
2023-01-15 10:39:04 +0900

    uninstall bazel

--- arch/setup.sh
+++ arch/setup.sh
@@ -40,7 +40,7 @@ yay --needed --removemake --S \
     arch-diff pacman-contrib pkgfile gdisk nfs-utils ntfs-3g neovim wget bat hexyl \
     openssh openconnect bash-completion words man ddrescue pdftk plantuml tmux rlwrap \
     xsel unzip evtest devmon hwinfo ntp nginx mariadb hping htop smartmontools tcpdump \
-    bazel cmake clang maven ninja nasm gdb cloc doxygen graphviz docker docker-compose docker-machine \
+    cmake clang maven ninja nasm gdb cloc doxygen graphviz docker docker-compose docker-machine \
     jre8-openjdk jdk8-openjdk tk ruby eigen scala sbt nodejs npm yarn rustup go dotnet-runtime \
     dstat logrotate lsof i2c-tools jq lhasa lzip p7zip protobuf dos2unix rsync
 
