57ed5b7f4504e54999aa8de77071dccb83143f0e
iTakeshi <itakeshi.net@gmail.com>
2023-04-19 09:39:12 +0900

    misc

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -165,18 +165,18 @@ return {
         }),
         preselect = cmp.PreselectMode.None,
       })
-      cmp.setup.cmdline('/', {
+      cmp.setup.cmdline("/", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
-          { name = 'buffer' }
+          { name = "buffer" }
         }
       })
-      cmp.setup.cmdline(':', {
+      cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
-          { name = 'path' }
+          { name = "path" }
         }, {
-          { name = 'cmdline' }
+          { name = "cmdline" }
         })
       })
     end,
