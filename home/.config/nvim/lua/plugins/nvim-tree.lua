dd4dc72af3f363ae5af3c21ab6de086fdadd1d1d
iTakeshi <itakeshi.net@gmail.com>
2023-04-29 12:09:45 +0900

    migrate to nvim-tree on_attach and fix move_to_parent behavior

--- home/.config/nvim/lua/plugins/nvim-tree.lua
+++ home/.config/nvim/lua/plugins/nvim-tree.lua
@@ -3,49 +3,58 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = "nvim-web-devicons",
     event = "VeryLazy",
+
     config = function()
       -- TODO: memorize tab-wise state
       local api = require("nvim-tree.api")
       local nvim_tree = require("nvim-tree")
+
+      local on_attach = function(bufnr)
+        local opts = function(desc)
+          return {
+            desc = "nvim-tree: " .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true,
+          }
+        end
+        local change_root_to_parent = function()
+          local line = vim.api.nvim_win_get_cursor(0)[1]
+          api.tree.change_root_to_parent()
+          local cursor = vim.api.nvim_win_get_cursor(0)
+          cursor[1] = cursor[1] - line + 1
+          vim.api.nvim_win_set_cursor(0, cursor)
+          api.tree.collapse_all()
+        end
+        vim.keymap.set("n", "e", api.node.open.edit, opts("Open"))
+        vim.keymap.set("n", "<cr>", api.node.open.edit, opts("Open"))
+        vim.keymap.set("n", "E", api.node.open.vertical, opts("Open: Vertical Split"))
+        vim.keymap.set("n", "o", api.node.run.system, opts("Run System"))
+        vim.keymap.set("n", "l", api.tree.change_root_to_node, opts("move into node"))
+        vim.keymap.set("n", "h", change_root_to_parent, opts("move to parent"))
+        vim.keymap.set("n", "t", api.node.open.edit, opts("Open"))
+        vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+        vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+        vim.keymap.set("n", "Y", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+        vim.keymap.set("n", "<c-n>", api.fs.create, opts("Create"))
+        vim.keymap.set("n", "<c-d>", api.fs.remove, opts("Delete"))
+        vim.keymap.set("n", "<c-x>", api.fs.cut, opts("Cut"))
+        vim.keymap.set("n", "<c-c>", api.fs.copy.node, opts("Copy"))
+        vim.keymap.set("n", "<c-v>", api.fs.paste, opts("Paste"))
+        vim.keymap.set("n", "<c-r>", api.fs.rename, opts("Rename"))
+        vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+        vim.keymap.set("n", "<c-f>", api.tree.close, opts("Close"))
+        vim.keymap.set("n", "<esc><esc>", api.tree.close, opts("Close"))
+      end
+
       nvim_tree.setup({
         git = { enable = false },
         remove_keymaps = true,
         view = {
           float = { enable = true },
-          mappings = {
-            list = {
-              { key = { "e", "<cr>" }, action = "edit" }, -- TODO: `drop` behavior
-              { key = "E", action = "vsplit" }, -- TODO: disable window picker
-              { key = "o", action = "system_open" },
-              { key = "l", action = "move into node", action_cb = api.tree.change_root_to_node },
-              {
-                key = "h",
-                action = "move to parent",
-                action_cb = function()
-                  local line = vim.api.nvim_win_get_cursor(0)[1]
-                  api.tree.change_root_to_parent()
-                  api.tree.collapse_all()
-                  local cursor = vim.api.nvim_win_get_cursor(0)
-                  cursor[1] = cursor[1] - line + 1
-                  vim.api.nvim_win_set_cursor(0, cursor)
-                end,
-              },
-              { key = "t", action = "edit" },
-              { key = "H", action = "toggle_dotfiles" },
-              { key = "y", action = "copy_name" },
-              { key = "Y", action = "copy_absolute_path" },
-              { key = "<c-n>", action = "create" },
-              { key = "<c-d>", action = "remove" },
-              { key = "<c-x>", action = "cut" },
-              { key = "<c-c>", action = "copy" },
-              { key = "<c-v>", action = "paste" },
-              { key = "<c-r>", action = "rename" },
-              { key = "q", action = "close" },
-              { key = "<c-f>", action = "close" },
-              { key = "<esc><esc>", action = "close" },
-            },
-          },
         },
+        on_attach = on_attach,
         renderer = {
           add_trailing = true,
           highlight_opened_files = "icon",
@@ -57,6 +66,7 @@ return {
           },
         },
       })
+
       local open_nvim_tree = function()
         local width = vim.fn.min({ vim.o.columns - 30, 120 })
         local height = vim.fn.min({ vim.o.lines - 8, 50 })
