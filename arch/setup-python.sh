112fc3585006e35deda6bf348f02d1ff55b9442b
iTakeshi <itakeshi.net@gmail.com>
2023-11-05 20:03:42 +0900

    remove venvs for neovim

--- arch/setup-python.sh
+++ arch/setup-python.sh
@@ -14,23 +14,6 @@ if [[ ! $(pyenv versions | grep $python3_latest) ]]; then
     pyenv install "$python3_latest"
 fi
 
-if [[ $(pyenv versions | grep neovim2) ]]; then
-    pyenv virtualenv-delete -f neovim2
-fi
-
-if [[ $(pyenv versions | grep neovim3) ]]; then
-    pyenv virtualenv-delete -f neovim3
-fi
-
-pyenv virtualenv "$python3_latest" neovim3
-pyenv global neovim3
-pyenv rehash
-pip install pynvim
-pyenv virtualenv "$python2_latest" neovim2
-pyenv global neovim2
-pyenv rehash
-pip2 install pynvim
-
 pyenv global "$python3_latest"
 pyenv rehash
 pip install numpy matplotlib PyQt5
